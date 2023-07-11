c474050966c7dedffd6189b510b15166f7f34d6e
Yehia Tarek Ragab <94568731+yehiatarek63@users.noreply.github.com>
2023-07-11 12:56:29 +0300

    Added Favourites Functionality

---
+++ XMLParser/Pages/Feed.cshtml.cs
@@ -0,0 +1,49 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Xml;
+
+
+namespace XMLParser.Pages;
+[BindProperties]
+public class FeedModel : PageModel
+{
+    private readonly IHttpClientFactory _httpClientFactory;
+    public List<FeedItem> FeedData { get; set; } = new List<FeedItem>();
+    public FeedModel(IHttpClientFactory httpClientFactory)
+    {
+        _httpClientFactory = httpClientFactory;
+    }
+    public async Task OnGet(string xmlUrl)
+    {
+        XmlDocument xmlDoc = new XmlDocument();
+        var client = _httpClientFactory.CreateClient();
+        HttpResponseMessage response = await client.GetAsync(xmlUrl);
+        if (response.IsSuccessStatusCode)
+        {
+            string content = await response.Content.ReadAsStringAsync();
+            xmlDoc.LoadXml(content);
+            foreach (var node in xmlDoc.SelectNodes("rss/channel/item").Cast<XmlNode>())
+            {
+                FeedItem item = new FeedItem();
+                if (node.SelectSingleNode("title") is not null)
+                {
+                    item.Title = node.SelectSingleNode("title").InnerText;
+                }
+                item.Description = node.SelectSingleNode("description").InnerText;
+                item.PubDate = node.SelectSingleNode("pubDate").InnerText;
+                item.Link = node.SelectSingleNode("link").InnerText;
+                item.Guid = node.SelectSingleNode("guid").InnerText;
+                FeedData.Add(item);
+            }
+        }
+    }
+}
+
+public class FeedItem
+{
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public string PubDate { get; set; }
+    public string Link { get; set; }
+    public string Guid { get; set; }
+}
--- XMLParser/Pages/Index.cshtml.cs
+++ XMLParser/Pages/Index.cshtml.cs
@@ -6,89 +6,130 @@ using System.Xml;
 using System.Xml.Serialization;
 using System.Xml.Linq;
 using System.Net.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
 
-namespace XMLParser.Pages
+namespace XMLParser.Pages;
+
+[BindProperties]
+public class IndexModel : PageModel
 {
-    [BindProperties]
-    public class IndexModel : PageModel
+    private readonly IHttpClientFactory _httpClientFactory;
+    public int PageIndex { get; set; } = 1;
+    public int PageSize { get; set; } = 5;
+    public List<string> FeedTitles { get; set; } = new List<string>();
+    public List<string> XmlUrls { get; set; } = new List<string>();
+    public List<string> HtmlUrls { get; set; } = new List<string>();
+    public int TotalPages { get; set; } = 0;
+    public IndexModel(IHttpClientFactory httpClientFactory)
     {
-        private readonly IHttpClientFactory _httpClientFactory;
-        public List<List<ItemProperties>> RssItems { get; set; } = new List<List<ItemProperties>>();
-        public int PageIndex { get; set; } = 1;
-        public int PageSize { get; set; } = 5;
-        public List<string> FeedTitles { get; set; } = new List<string>();
-        public List<string> XmlUrls { get; set; } = new List<string>();
-        public List<string> HtmlUrls { get; set; } = new List<string>();
-        public IndexModel(IHttpClientFactory httpClientFactory)
-        {
-            _httpClientFactory = httpClientFactory;
-        }
+        _httpClientFactory = httpClientFactory;
+    }
 
-        public async Task OnGet(int? pageIndex)
+    public async Task OnGet(int? pageIndex)
+    {
+        if (pageIndex is not null)
         {
-            if(pageIndex is not null)
-            {
-                PageIndex = (int)pageIndex;
-            }
-            XmlDocument xmlDoc = new XmlDocument();
-            var client = _httpClientFactory.CreateClient();
-            (HtmlUrls, FeedTitles, XmlUrls) = await GetOutline(client, "https://blue.feedland.org/opml?screenname=dave");
-            var paginatedXMLURLs = XmlUrls.Skip((PageIndex - 1) * PageSize).Take(PageSize).ToList();
-            FeedTitles = FeedTitles.Skip((PageIndex - 1) * PageSize).Take(PageSize).ToList();
-            HtmlUrls = HtmlUrls.Skip((PageIndex - 1) * PageSize).Take(PageSize).ToList();
-            foreach (var url in paginatedXMLURLs)
-            {
-               List<ItemProperties> ItemsProperties = new List<ItemProperties>();
-               HttpResponseMessage response = await client.GetAsync(url);
-               if (response.IsSuccessStatusCode)
-               {
-                   string content = await response.Content.ReadAsStringAsync();
-                   xmlDoc.LoadXml(content);
-                   foreach (var node in xmlDoc.SelectNodes("rss/channel/item").Cast<XmlNode>())
-                   {
-                       ItemProperties item = new ItemProperties();
-                       if (node.SelectSingleNode("title") is not null)
-                       {
-                           item.Title = node.SelectSingleNode("title").InnerText;
-                       }
-                       item.Description = node.SelectSingleNode("description").InnerText;
-                       item.PubDate = node.SelectSingleNode("pubDate").InnerText;
-                       item.Link = node.SelectSingleNode("link").InnerText;
-                       item.Guid = node.SelectSingleNode("guid").InnerText;
-                       ItemsProperties.Add(item);
-                   }
-                   RssItems.Add(ItemsProperties);
-               }
-            }
+            PageIndex = (int)pageIndex;
         }
-        
+        XmlDocument xmlDoc = new XmlDocument();
+        var client = _httpClientFactory.CreateClient();
+        (HtmlUrls, FeedTitles, XmlUrls) = await GetOutline(client, "https://blue.feedland.org/opml?screenname=dave");
+        TotalPages = (int)Math.Ceiling((double)XmlUrls.Count / PageSize);
+        XmlUrls = XmlUrls.Skip((PageIndex - 1) * PageSize).Take(PageSize).ToList();
+        FeedTitles = FeedTitles.Skip((PageIndex - 1) * PageSize).Take(PageSize).ToList();
+        HtmlUrls = HtmlUrls.Skip((PageIndex - 1) * PageSize).Take(PageSize).ToList();
+    }
+    public IActionResult OnPostStar(string xmlUrl, string htmlUrl, string feedTitle, int pageIndex)
+    {
+        string currentValue = Request.Cookies["XmlUrl"];
+        if (currentValue is not null)
+        {
+            currentValue += "," + xmlUrl;
+        }
+        else
+        {
+            currentValue = xmlUrl;
+        }
+        Response.Cookies.Append("XmlUrl", currentValue, new CookieOptions
+        {
+            Secure = true,
+        });
+        string currentFeedTitle = Request.Cookies["FeedTitle"];
+        if (currentFeedTitle is not null)
+        {
+            currentFeedTitle += "," + feedTitle;
+        }
+        else
+        {
+            currentFeedTitle = feedTitle;
+        }
+        Response.Cookies.Append("FeedTitle", currentFeedTitle, new CookieOptions
+        {
+            Secure = true,
+        });
+        string currentHtmlUrl = Request.Cookies["HtmlUrl"];
+        if (currentHtmlUrl is not null)
+        {
+            currentHtmlUrl += "," + htmlUrl;
+        }
+        else
+        {
+            currentHtmlUrl = htmlUrl;
+        }
+        Response.Cookies.Append("HtmlUrl", currentHtmlUrl, new CookieOptions
+        {
+            Secure = true
+        });
+        return RedirectToPage("Index", new { PageIndex = pageIndex });
+    }
+
+    public IActionResult OnPostDeleteStar(string xmlUrl, string htmlUrl, string feedTitle, int pageIndex)
+    {
+        if (Request.Cookies["XmlUrl"] is not null && Request.Cookies["HtmlUrl"] is not null && Request.Cookies["FeedTitle"] is not null)
+        {
+            List<string> xmlUrlList = Request.Cookies["XmlUrl"].Split(',').ToList();
+            List<string> htmlUrlList = Request.Cookies["HtmlUrl"].Split(',').ToList();
+            List<string> feedTitleList = Request.Cookies["FeedTitle"].Split(',').ToList();
+            xmlUrlList.Remove(xmlUrl);
+            htmlUrlList.Remove(htmlUrl);
+            feedTitleList.Remove(feedTitle);
+            Response.Cookies.Append("XmlUrl", string.Join(",", xmlUrlList), new CookieOptions
+            {
+                Secure = true
+            });
+            Response.Cookies.Append("HtmlUrl", string.Join(",", htmlUrlList), new CookieOptions
+            {
+                Secure = true
+            });
+            Response.Cookies.Append("FeedTitle", string.Join(",", feedTitleList), new CookieOptions
+            {
+                Secure = true
+            });
+        }
+        return RedirectToPage("Index", new {PageIndex = pageIndex});
+    }
 
 
-        async Task<(List<string>, List<string>, List<string>)> GetOutline(HttpClient client, string url)
+
+    async Task<(List<string>, List<string>, List<string>)> GetOutline(HttpClient client, string url)
+    {
+        HttpResponseMessage response = await client.GetAsync(url);
+        List<string> htmlUrls = new List<string>();
+        List<string> title = new List<string>();
+        List<string> xmlUrls = new List<string>();
+        if (response.IsSuccessStatusCode)
         {
-            HttpResponseMessage response = await client.GetAsync(url);
-            List<string> htmlUrls = new List<string>();
-            List<string> title = new List<string>();
-            List<string> xmlUrls = new List<string>();
-            if (response.IsSuccessStatusCode)
-            {
-                string content = await response.Content.ReadAsStringAsync();
-                XDocument doc = XDocument.Parse(content);
-                htmlUrls = doc.Descendants("outline").Select(x => x.Attribute("htmlUrl").Value).ToList();
-                title = doc.Descendants("outline").Select(x => x.Attribute("text").Value).ToList();
-                xmlUrls = doc.Descendants("outline").Select(x => x.Attribute("xmlUrl").Value).ToList();
-            }
-            return (htmlUrls, title, xmlUrls);
+            string content = await response.Content.ReadAsStringAsync();
+            XDocument doc = XDocument.Parse(content);
+            htmlUrls = doc.Descendants("outline").Select(x => x.Attribute("htmlUrl").Value).ToList();
+            title = doc.Descendants("outline").Select(x => x.Attribute("text").Value).ToList();
+            xmlUrls = doc.Descendants("outline").Select(x => x.Attribute("xmlUrl").Value).ToList();
         }
+        return (htmlUrls, title, xmlUrls);
     }
 }
 
+    
 
-public class ItemProperties
-{
-    public string Title { get; set; }
-    public string Description { get; set; }
-    public string PubDate { get; set; }
-    public string Link { get; set; }
-    public string Guid { get; set; }
-}
+
+
